4d0d46b61333a7029cc3ab4f9e9ec5b9e24219af
sogood17 <sogoodpiano@gmail.com>
2023-05-16 20:27:28 +0900

    mainpage

---
+++ src/components/MainBookmark.js
@@ -0,0 +1,13 @@
+import {React} from 'react';
+
+export default function MainBookmark({img, id, title, price, subTitle, bookmark, bookmarkHandler}) {
+    return(
+    <div className='product-container' key={id}>
+        <img className='product-img' src={img} alt={title}/>
+        <div className='bookmark-img-wrap'><img className='bookmark-img' src={bookmark? '../bookmark_on.png':'../bookmark_off.png'} onClick={bookmarkHandler} alt='bookmark'/></div>
+        <div className='title-wrap'><span className='product-title'><strong>{title}</strong></span></div>
+        <div className='title-wrap'><span className='product-sub-title'>{subTitle}</span><span className='price'>{price}</span></div>
+
+    </div>
+    )
+}
